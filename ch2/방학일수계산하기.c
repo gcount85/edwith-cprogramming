249d9f87fc6e914b159bd2544614f9e2c791b66f
gcount85 <100mgml@gmail.com>
2022-11-25 15:03:05 +0900

    ch2 방학 일수 계산하기

---
+++ ch2/방학일수계산하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int summer;
+    int winter;
+    int sum;
+
+    summer = 28;
+    winter = 38;
+    sum = summer + winter;
+
+    printf("%d\n", sum); 
+
+    return 0;
+}
